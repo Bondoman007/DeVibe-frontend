172a996091f3d4f402e9494d9bc9f16d9b5bc4f9
Bondoman007 <kanishkshrivastava037@gmail.com>
2025-06-17 16:59:42 +0530

    change

--- src/utils/socket.jsx
+++ src/utils/socket.jsx
@@ -2,7 +2,7 @@ import io from "socket.io-client";
 
 export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
-    return io("http://localhost:5000"); // assuming dev server
+    return io("http://localhost:3000"); // assuming dev server
   } else {
     return io("https://devibe-backend.onrender.com", {
       path: "/socket.io", // default socket.io path; change only if backend uses a custom path
